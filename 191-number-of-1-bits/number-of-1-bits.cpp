a2f646ddac0898d1e33ce468bf2541d4c1218190
Aadil Farooq <aadilfarooq284@gmail.com>
2026-08-06 10:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (49.31%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+        while(n !=0){
+      count=count+(n&1);
+
+       n= (n >> 1); // (>)triple nahi hoga
+
+        }
+        return count;
+        
+        
+    }
+};
